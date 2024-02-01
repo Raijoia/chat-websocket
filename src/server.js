cd7fb4bcb08e4aa20a2dcf248b28f48c5fecf58a
Raí Joia <megac154@gmail.com>
2024-02-01 11:54:28 -0300

    feat: add socket.io e criando o servidor de evento

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +12,13 @@ const diretorio = path.join(caminhoAtual, "../..", "public");
 
 app.use(express.static(diretorio));
 
-app.listen(PORT, () => {
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(PORT, () => {
   console.log(`Porta => ${PORT}`)
+})
+
+const io = new Server(servidorHttp);
+io.on("connection", (socket) => {
+  console.log("Usuário conectado");
 })
